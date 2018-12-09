d4e25fee9b0d85e2161429c5ac9c95ff06c3cacb
AMCzajka <45639106+AMCzajka@users.noreply.github.com>
2018-12-09 21:05:50 +0100

    Create Role.java

---
+++ Role.java
@@ -0,0 +1,48 @@
+package com.monitoring.entity;
+
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.TABLE)
+	private Integer idRoli;
+	
+	private String typRoli;
+	
+	public String getTypRoli() {
+		return typRoli;
+	}
+
+	public void setTypRoli(String typRoli) {
+		this.typRoli = typRoli;
+	}
+
+	@ManyToMany(mappedBy="role")
+	private List<Uzytkownicy> uzytkownicy;
+
+	public Integer getIdRoli() {
+		return idRoli;
+	}
+
+	public void setIdRoli(Integer idRoli) {
+		this.idRoli = idRoli;
+	}
+
+	public List<Uzytkownicy> getUzytkownicy() {
+		return uzytkownicy;
+	}
+
+	public void setUzytkownicy(List<Uzytkownicy> uzytkownicy) {
+		this.uzytkownicy = uzytkownicy;
+	}
+	
+}
