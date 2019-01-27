ceedb33732ea0ed3d01fcbbbf54862bd0dd79821
AMCzajka <45639106+AMCzajka@users.noreply.github.com>
2019-01-27 20:45:59 +0100

    Create UzytkownicyRepository.java

---
+++ UzytkownicyRepository.java
@@ -0,0 +1,9 @@
+package com.rafbur.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rafbur.entity.Uzytkownicy;
+
+public interface UzytkownicyRepository extends JpaRepository<Uzytkownicy, Integer>{
+
+}
