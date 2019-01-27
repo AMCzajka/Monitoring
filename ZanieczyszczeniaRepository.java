0889b81ebe35b248150fe487248cafec621ae491
AMCzajka <45639106+AMCzajka@users.noreply.github.com>
2019-01-27 20:46:24 +0100

    Create ZanieczyszczeniaRepository.java

---
+++ ZanieczyszczeniaRepository.java
@@ -0,0 +1,9 @@
+package com.rafbur.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rafbur.entity.Zanieczyszczenia;
+
+public interface ZanieczyszczeniaRepository extends JpaRepository<Zanieczyszczenia, Integer> {
+
+}
